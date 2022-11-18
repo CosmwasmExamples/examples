586ecd0720398b0e45173985dd9cbbc5c74bb262
CosmWasm <cosmwasm@gmail.com>
2022-11-19 00:04:23 +0700

    init contract

---
+++ contracts/osmosis-swap/src/contract.rs
@@ -0,0 +1,95 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Reply, Response, StdResult};
+use cw2::set_contract_version;
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, MigrateMsg};
+
+use osmosis_std::types::cosmos::base::v1beta1::Coin;
+use osmosis_std::types::osmosis::gamm::v1beta1::{
+    MsgSwapExactAmountIn, SwapAmountInRoute,
+};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:osmosis-swap";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Handling contract instantiation
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender))
+}
+
+/// Handling contract execution
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    match msg {
+        ExecuteMsg::Swap {
+            pool_id, amount_out_min, denom_out
+        } => execute::swap(deps, env, info, pool_id, amount_out_min, denom_out),
+    }
+}
+
+pub mod execute {
+    use cosmwasm_std::CosmosMsg;
+
+    use super::*;
+
+    pub fn swap(_deps: DepsMut, env: Env, info: MessageInfo, pool_id: u64, amount_out_min: String, denom_out: String) -> Result<Response, ContractError> {
+        if info.funds.is_empty() {
+            return Err(ContractError::InvalidFund {});
+        }
+        let fund = info.funds[0].clone();
+        let route = SwapAmountInRoute {
+            pool_id,
+            token_out_denom: denom_out,
+        };
+        let swap_msg: CosmosMsg = MsgSwapExactAmountIn {
+            sender: env.contract.address.to_string(),
+            routes: vec![route],
+            token_in: Some(Coin { denom: fund.denom, amount: fund.amount.to_string() }),
+            token_out_min_amount: amount_out_min
+        }.into();
+
+        Ok(Response::new()
+            .add_attribute("method", "swap")
+            .add_message(swap_msg))
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> StdResult<Response> {
+    // perform state update or anything neccessary for the migration
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+    }
+}
+
+/// Handling submessage reply.
+/// For more info on submessage and reply, see https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#submessages
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn reply(_deps: DepsMut, _env: Env, _msg: Reply) -> Result<Response, ContractError> {
+    // With `Response` type, it is still possible to dispatch message to invoke external logic.
+    // See: https://github.com/CosmWasm/cosmwasm/blob/main/SEMANTICS.md#dispatching-messages
+
+    todo!()
+}
---
+++ contracts/osmosis-swap/src/msg.rs
@@ -0,0 +1,30 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+/// Message type for `instantiate` entry_point
+#[cw_serde]
+pub struct InstantiateMsg {}
+
+/// Message type for `execute` entry_point
+#[cw_serde]
+pub enum ExecuteMsg {
+    Swap {
+        pool_id: u64,
+        amount_out_min: String,
+        denom_out: String,
+    },
+}
+
+#[cw_serde]
+pub struct MigrateMsg {}
+
+/// Message type for `query` entry_point
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    // #[returns(YourQueryResponse)]
+    // YourQuery {},
+}
+
+// We define a custom struct for each query response
+// #[cw_serde]
+// pub struct YourQueryResponse {}
